0c37f018436b448de5eeaaa966916a62a3d02d82
visal <vtob19@aol.com>
2021-09-08 21:22:26 -0700

    created window, clock, and initial game loop

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import pygame, sys
-
 from pygame.locals import *
-pygame.init() # initiates pygame
 
-window_size = (400,400)
+''' clock '''
+clock = pygame.time.Clock()
 
+# initiates pygame
+pygame.init()
+
+# initiate window
+window_size = (1000,800)
 screen = pygame.display.set_mode(window_size, 0, 32)
+pygame.display.set_caption("Wilt-Blizzard Dynasty")
+
+''' game loop '''
+while True:
+
+    # key board events
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    # sets game to 60 fps
+    clock.tick(60)
